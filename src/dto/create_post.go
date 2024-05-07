51193c2adcb3c79cba4944da6e3e5a3b3c5cbb3a
João <joao.gabsilva1007@gmail.com>
2024-05-07 20:06:36 -0300

    fix: folder structure to monorepo

--- src/dto/create_post.go
+++
@@ -1,6 +0,0 @@
-package dto
-
-type CreatePost struct {
-	Title string `json:"title"`
-	Content string `json:"content"`
-}
--- src/dto/update_password.go
+++
@@ -1,6 +0,0 @@
-package dto
-
-type UpdatePassword struct {
-	CurrentPassword string `json:"currentPassword"`
-	NewPassowrd string `json:"newPassword"`
-}
